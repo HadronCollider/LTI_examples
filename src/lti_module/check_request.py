0a3d876b1e8b46116fd677a3715c7d830c614bd6
Dmitry Ivanov <darcenrall@gmail.com>
2020-12-07 13:39:59 +0300

    rm utils

--- src/lti_module/check_request.py
+++ src/lti_module/check_request.py
@@ -1,12 +1,11 @@
 from lti.tool_provider import ToolProvider
 from .lti_validator import LTIRequestValidator
-from .utils import check_content
 
 
 def check_request(request_info):
-    # request_info must contains keys: ('headers', 'data', 'url', 'secret')
-    check_content(request_info)
-    
+    """
+    :request_info: dcit - must include ('headers', 'data', 'secret', 'url') 
+    """
     provider = ToolProvider.from_unpacked_request(
         secret=request_info.get('secret', None),
         params=request_info.get('data', {}),
--- src/lti_module/utils.py
+++
@@ -1,6 +0,0 @@
-REQUEST_INFO = ('headers', 'data', 'url', 'secret')
-
-
-def check_content(data):
-    for key in REQUEST_INFO:
-        if key not in data: raise KeyError("Required param '{}' not in {}".format(key, data))
